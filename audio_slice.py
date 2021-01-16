17e27e08b70f85ab2b1d4781dd10841c0c7eae65
taru <taruru1010@gmail.com>
2021-01-17 06:02:41 +0900

    保存しているwavファイルの最後の10秒を永遠にトリミングし続けるコード

---
+++ audio_slice.py
@@ -0,0 +1,21 @@
+from pydub import AudioSegment
+from time import sleep
+
+
+# timeで指定した分だけ、音声をスライスする処理。　eg: 10000=10秒
+def extracting_from_last(time=10000, import_file="./example.wav", export_file="output.wav"):
+    time = -time
+    sound = AudioSegment.from_file(import_file, format="wav")
+    sliced_sound = sound[time:]
+    sliced_sound.export(export_file, format="wav")
+    print("completed slice!")
+
+def loop_extract_loop():
+    sleep_time = 10
+    while True:
+        extracting_from_last()
+        sleep(sleep_time)
+
+if __name__ == "__main__":
+    # extracting_from_last()
+    loop_extract_loop()
