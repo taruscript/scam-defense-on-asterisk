3aefc652189a02e2a47546e163711dee5cb82c07
taru <taruru1010@gmail.com>
2021-01-17 06:44:25 +0900

    スライス済みの音声ファイルをpostしてくれる処理を追加

--- audio_slice.py
+++ audio_slice.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
 from time import sleep
+import requests
 
 
 # timeで指定した分だけ、音声をスライスする処理。　eg: 10000=10秒
@@ -10,12 +11,23 @@ def extracting_from_last(time=10000, import_file="./example.wav", export_file=".
     sliced_sound.export(export_file, format="wav")
     print("completed slice!")
 
+
+# スライス済みの音声ファイルをpostし特殊詐欺を解析する。
+def post_wav_extracted():
+    fileName = './output.wav'
+    files = {'file': open(fileName, 'rb')}
+    r = requests.post("http://localhost:8888/scam_check", files=files)
+    print(r.json())  
+
+# 10秒録音して、10秒待機するという処理を永遠に繰り返す
 def loop_extract_loop():
     sleep_time = 10
     import_file = "/var/spool/asterisk/monitor/record-in.wav" 
     while True:
         extracting_from_last(import_file=import_file)
+        post_wav_extracted()
         sleep(sleep_time)
+  
 
 if __name__ == "__main__":
     # extracting_from_last()
