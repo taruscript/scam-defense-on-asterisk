d2b0cc62ce0f26045aa103755cca5e3e06f56ecf
ubuntu <ubuntu@ik1-447-55780.vs.sakura.ne.jp>
2021-01-17 06:33:36 +0900

    読み込む音声ファイルパスを指定

--- audio_slice.py
+++ audio_slice.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 # timeで指定した分だけ、音声をスライスする処理。　eg: 10000=10秒
-def extracting_from_last(time=10000, import_file="./example.wav", export_file="output.wav"):
+def extracting_from_last(time=10000, import_file="./example.wav", export_file="./output.wav"):
     time = -time
     sound = AudioSegment.from_file(import_file, format="wav")
     sliced_sound = sound[time:]
@@ -12,8 +12,9 @@ def extracting_from_last(time=10000, import_file="./example.wav", export_file="o
 
 def loop_extract_loop():
     sleep_time = 10
+    import_file = "/var/spool/asterisk/monitor/record-in.wav" 
     while True:
-        extracting_from_last()
+        extracting_from_last(import_file=import_file)
         sleep(sleep_time)
 
 if __name__ == "__main__":
