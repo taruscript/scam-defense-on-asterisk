17cc0cd4486a79666e7d6bfc558e870f27458e8e
ubuntu <ubuntu@ik1-447-55780.vs.sakura.ne.jp>
2021-01-18 04:04:36 +0900

    wavファイルをwavに変換して、そのファイルに対してトリミング処理を行う

--- audio_slice.py
+++ audio_slice.py
@@ -3,23 +3,26 @@ from time import sleep
 import requests
 from scipy.io import wavfile
 from pydub.utils import mediainfo
-
+import shutil
+import os
 
 
 # timeで指定した分だけ、音声をスライスする処理。　eg: 10000=10秒
-def extracting_from_last(time=10, import_file="./example.wav", export_file="./output.wav"):
+def extracting_from_last(time=10000, import_file="./example.wav", export_file="./output.wav"):
+    os.system(f'ffmpeg -y -i "{import_file}" -ar 44100 "output2.wav"')
     time = -time
-    # sound = AudioSegment.from_file(import_file, format="wav")
-    # sliced_sound = sound[time:]
-    # sliced_sound.export(export_file, format="wav")
-    # print("completed slice!")
-    current_time = float(mediainfo(import_file)["duration"])
-    trimming_calc = current_time - time
-    sampleRate, waveData = wavfile.read(import_file)
-    startSample = int(trimming_calc  * sampleRate )
-    endSample = int( current_time * sampleRate )
-    wavfile.write( export_file, sampleRate, waveData[startSample:endSample])
+    sound = AudioSegment.from_file("output2.wav", format="wav")
+    sliced_sound = sound[time:]
+    sliced_sound.export("output2.wav", format="wav")
     print("completed slice!")
+
+    # current_time = float(mediainfo(import_file)["duration"])
+    # trimming_calc = current_time - time
+    # sampleRate, waveData = wavfile.read(import_file)
+    # startSample = int(trimming_calc  * sampleRate )
+    # endSample = int( current_time * sampleRate )
+    # wavfile.write( export_file, sampleRate, waveData[startSample:endSample])
+    # print("completed slice!")
 
 
 
@@ -33,17 +36,18 @@ def extracting_from_last(time=10, import_file="./example.wav", export_file="./ou
 
 # スライス済みの音声ファイルをpostし特殊詐欺を解析する。
 def post_wav_extracted():
-    fileName = './output.wav'
+    fileName = './output2.wav'
     files = {'file': open(fileName, 'rb')}
     r = requests.post("http://localhost:8888/scam_check", files=files)
     print(r.json())  
 
 # 10秒録音して、10秒待機するという処理を永遠に繰り返す
 def loop_extract_loop():
-    sleep_time = 10
+    sleep_time = 5
     import_file = "/var/spool/asterisk/monitor/record-in.wav" 
     while True:
-        extracting_from_last(import_file=import_file)
+        shutil.copyfile(import_file, "./output.wav")
+        extracting_from_last(import_file="./output.wav")
         post_wav_extracted()
         sleep(sleep_time)
   
