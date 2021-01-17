fc3c19fdb05a3ab8835ba8ae3a420c1d2275c80c
taru <taruru1010@gmail.com>
2021-01-18 02:59:50 +0900

    ファイルをトリミングする処理で変更を加えた

--- audio_slice.py
+++ audio_slice.py
@@ -1,15 +1,34 @@
 from pydub import AudioSegment
 from time import sleep
 import requests
+from scipy.io import wavfile
+from pydub.utils import mediainfo
+
 
 
 # timeで指定した分だけ、音声をスライスする処理。　eg: 10000=10秒
-def extracting_from_last(time=10000, import_file="./example.wav", export_file="./output.wav"):
+def extracting_from_last(time=10, import_file="./example.wav", export_file="./output.wav"):
     time = -time
-    sound = AudioSegment.from_file(import_file, format="wav")
-    sliced_sound = sound[time:]
-    sliced_sound.export(export_file, format="wav")
+    # sound = AudioSegment.from_file(import_file, format="wav")
+    # sliced_sound = sound[time:]
+    # sliced_sound.export(export_file, format="wav")
+    # print("completed slice!")
+    current_time = float(mediainfo(import_file)["duration"])
+    trimming_calc = current_time - time
+    sampleRate, waveData = wavfile.read(import_file)
+    startSample = int(trimming_calc  * sampleRate )
+    endSample = int( current_time * sampleRate )
+    wavfile.write( export_file, sampleRate, waveData[startSample:endSample])
     print("completed slice!")
+
+
+
+# def trim_wav(originalWavPath, newWavPath , start, end ):
+
+ 
+ 
+# wp = r"pathToWav.wav"
+# trim_wav(wp, wp.replace(".wav", "_trimmed.wav"), 0,10)
 
 
 # スライス済みの音声ファイルをpostし特殊詐欺を解析する。
