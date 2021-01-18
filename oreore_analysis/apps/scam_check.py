649f2cf9ef1bfbf9695ac5efb6860bf000ed81d7
taru <taruru1010@gmail.com>
2021-01-18 19:10:48 +0900

    自然言語処理で近しいことばをスコア化出来ない問題を解決した

--- oreore_analysis/apps/scam_check.py
+++ oreore_analysis/apps/scam_check.py
@@ -11,6 +11,7 @@ app = Blueprint('input_data', __name__)
 model = KeyedVectors.load_word2vec_format('apps/word_model/entity_vector.model.bin', binary=True)
 # model = KeyedVectors.load_word2vec_format('apps/word_model/entity_vector.model.bin')
 tagger = MeCab.Tagger("-d /var/lib/mecab/dic/ipadic")
+# tagger = MeCab.Tagger("-d /usr/local/lib/mecab/dic/ipadic/")
 tagger.parse("")
 
 
@@ -27,14 +28,14 @@ def input_data():
     save_path = f"upload/{fs.filename}"
     fs.save(save_path)
 
-    
     audio_recognized = voice_recognize(save_path)
 
     if audio_recognized is None:
         return jsonify({"msg": "fail"})
     scam_result =  scam_check(audio_recognized)
     # return jsonify(scam_result)
-    return jsonify({"msg":"sucess"})
+    return jsonify({"msg":"sucess", "result": scam_result})
+
 
 def voice_recognize(save_path):
     print("音声認識を開始します") 
@@ -48,6 +49,7 @@ def voice_recognize(save_path):
         return None
     return result_recognized
 
+
 # 特殊詐欺を検知する
 def scam_check(text):
     print(text)
@@ -57,22 +59,27 @@ def scam_check(text):
     word = []
     # 特殊詐欺に使われそうな語群
     keywords = ['銀行','警察']
+    # keywords = ["泡", "石鹸"]
     while node is not None:
         fields = node.feature.split(",")
         if fields[0] == '名詞' or fields[0] == '動詞' or fields[0] == '形容詞':   
             try:
                 for keyword in keywords:
-                    scores.append(model.wv.similarity(node.surface, keyword))
-                word.append(node.surface)
+                    #　単語とkeywordの類似度
+                    similarity_score = model.wv.similarity(node.surface, keyword)
+                    scores.append(similarity_score)
+                    word.append(node.surface)
             except KeyError:
-                pass
-            
+                pass 
         node = node.next
-    # highcore_index = scores.index(max(scores)) // len(keywords)
-    # highscore_word = word[highcore_index]
 
-    # print(highscore_word)
-    # print(max(scores))
-    # return {"score": max(scores), "high_extraction": highscore_word}
-    print(word)
-    return {"test": "test"}
+    highscore = max(scores)
+    highscore_index = scores.index(highscore) // len(keywords)
+    highscore_word = word[highscore_index]
+    
+    # 類似度が70％以上でないと、特殊詐欺に扱われる言葉として認定出来ない
+    if highscore >= 0.7:
+        return {"score": str(highscore), "word": str(highscore_word)}
+    else:
+        return {"score": 0}
+
